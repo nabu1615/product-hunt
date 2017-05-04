fa35c33392f1ca8ee93ca9c0a330f5cfbd460154
Javier Vargas <javier1615@gmail.com>
2017-05-04 17:01:31 -0500

    create-show-view-for-products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,10 @@ class ProductsController < ApplicationController
 		end
 	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
 	private
 		def products_params
 			params.require(:product).permit(:name, :url, :description)
