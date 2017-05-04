0029337fd67a8aa28ba60fdc33e91b971ab19c9a
Javier Vargas <javier1615@gmail.com>
2017-05-04 14:38:22 -0500

    adding-route-new-for-products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,18 @@ class ProductsController < ApplicationController
 	def new
 		@product = Product.new
 	end
+
+	def create
+		@product = Product.new(products_params)
+		if @product.save
+			redirect_to products_path, notice: 'El Producto fue publicado con exito'
+		else
+			render :new
+		end
+	end
+
+	private
+		def products_params
+			params.require(:product).permit(:name, :url, :description)
+		end
 end
